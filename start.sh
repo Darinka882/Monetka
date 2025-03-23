5f827f13d55e5cc5b538ae8e6153fe33cbb47138
Darinka882 <darina.samosidiva@gmail.com>
2025-03-23 18:20:29 +0300

    Fix webhook app setup

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pip install -r requirements.txt
-exec gunicorn bot:app --worker-class aiohttp.GunicornWebWorker --bind 0.0.0.0:$PORT
-
+gunicorn bot:main --worker-class aiohttp.GunicornWebWorker --bind 0.0.0.0:$PORT
 
 
